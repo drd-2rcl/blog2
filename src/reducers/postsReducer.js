c3374cddc3de7d0028f4f8524083c156188a0922
drd-2rcl <drd.rclrd@gmail.com>
2019-05-01 22:19:48 -0300

    pure reducers

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,3 +1,10 @@
-export default () => {
-  return 123;
+export default (state, action) => {
+  // BAD!
+  // return document.querySelector('input');
+
+  // BAD!
+  // return axios.get('/posts')
+
+  // GOOD!
+  return state + action
 }
