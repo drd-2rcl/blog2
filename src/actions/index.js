1fae81688b8daa04c05bd885cbf6bc47e71d0f43
drd-2rcl <drd.rclrd@gmail.com>
2019-05-02 17:41:31 -0300

    include memoize function

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,10 +3,20 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
-
   dispatch({ type: 'FETCH_POSTS', payload: response.data })
 };
 
+// INCLUDE MEMOIZE
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+// CREATE PRIVATE CONST
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+}); 
+
+
+// ORIGINAL CODE
 // export const fetchUser = id => async dispatch => {
 //   const response = await jsonPlaceholder.get(`/users/${id}`);
   
@@ -14,10 +24,10 @@ export const fetchPosts = () => async dispatch => {
 // };
 
 // REFACTORING TO LODASH MEMOIZE FUNCTION
-export const fetchUser = function(id) {
-  return _.memoize(async function(dispatch) {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
+// export const fetchUser = function(id) {
+//   return _.memoize(async function(dispatch) {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
     
-    dispatch({ type: 'FETCH_USER', payload: response.data });
-  });
-};
+//     dispatch({ type: 'FETCH_USER', payload: response.data });
+//   });
+// };
