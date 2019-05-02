c7b0ab273c04cee1eb14b3614759b330e64a4aec
drd-2rcl <drd.rclrd@gmail.com>
2019-05-02 17:52:23 -0300

    alternate overfetching solution

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,15 +6,20 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: 'FETCH_POSTS', payload: response.data })
 };
 
-// INCLUDE MEMOIZE
-export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
-
-// CREATE PRIVATE CONST
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({ type: 'FETCH_USER', payload: response.data });
-}); 
+}; 
 
+
+// // INCLUDE MEMOIZE
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+// // CREATE PRIVATE CONST
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// }); 
 
 // ORIGINAL CODE
 // export const fetchUser = id => async dispatch => {
