44bb0d0ed6129d757a41b11305c9f36ee2a62322
drd-2rcl <drd.rclrd@gmail.com>
2019-05-02 17:29:42 -0300

    refactoring for _.memoize func

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,23 @@
+import _ from 'lodash'
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
 
   dispatch({ type: 'FETCH_POSTS', payload: response.data })
-}
+};
 
-export const fetchUser = id => async dispatch => {
-  const response = await jsonPlaceholder.get(`/users/${id}`);
+// export const fetchUser = id => async dispatch => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
   
-  dispatch({ type: 'FETCH_USER', payload: response.data });
-}
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// };
 
+// REFACTORING TO LODASH MEMOIZE FUNCTION
+export const fetchUser = function(id) {
+  return _.memoize(async function(dispatch) {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+  });
+};
