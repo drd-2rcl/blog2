a4431fde939bd65bc5e96793e69b6180d80cdc10
drd-2rcl <drd.rclrd@gmail.com>
2019-05-01 22:35:06 -0300

    rules and mutations

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -5,6 +5,18 @@ export default (state, action) => {
   // BAD!
   // return axios.get('/posts')
 
+  // BAD!
+  // state[0] = 'Sam'
+  // state.pop()
+  // state.push()
+  // state.name = 'Same'
+  // state.age = 30
+
+  // MUST NOT MUTATE ITS INPUT 'STATE' ARGUMENT
+  // inside JS, strings and numbers are immutable values, unless inside object or array;
+
   // GOOD!
   return state + action
+
+
 }
