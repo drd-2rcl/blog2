3199139dcea4c453843fadeee4181785df1c6c68
drd-2rcl <drd.rclrd@gmail.com>
2019-05-02 11:33:24 -0300

    refactoring for switch case action payload

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,22 +1,8 @@
-export default (state, action) => {
-  // BAD!
-  // return document.querySelector('input');
-
-  // BAD!
-  // return axios.get('/posts')
-
-  // BAD!
-  // state[0] = 'Sam'
-  // state.pop()
-  // state.push()
-  // state.name = 'Same'
-  // state.age = 30
-
-  // MUST NOT MUTATE ITS INPUT 'STATE' ARGUMENT
-  // inside JS, strings and numbers are immutable values, unless inside object or array;
-
-  // GOOD!
-  return state + action
-
-
+export default (state = [], action) => {
+  switch (action.type) {
+    case 'FETCH_POSTS':
+      return action.payload;
+    default: 
+      return state;
+  }
 }
